5d035c6cebcd09bc26ea154bd22300f5c391627f
nc-minh <nguyencongminh.official@gmail.com>
2022-04-05 10:59:35 +0700

    :revolving_hearts: basic command

---
+++ basic_command/basic_command.py
@@ -0,0 +1,22 @@
+# Line hyphen - \
+# Block code
+# Quotation (Quotations can be used for code comments)
+quotation_1 = 'This is quotation!'
+quotation_2 = "This is Ming's quotation!"
+quotation_3 = '''We don't
+talk
+anymore
+!!!
+'''
+print(quotation_1)
+print(quotation_2)
+print(quotation_3)
+
+# Data type
+'''
+- Number
+- String
+- List
+- Tuple
+- Dictionary
+'''
